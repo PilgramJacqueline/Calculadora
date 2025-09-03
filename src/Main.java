32e03a84b7f10b4484ac598bd36f3db525432897
Admin <20243ds007@utez.edu.mx>
2025-09-02 19:56:33 -0600

    error al sumar

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("==calculadora de suma==:");
+
+        System.out.println("ingresa el numero uno");
+        double numero1 = sc.nextDouble();
+        System.out.println("ingresa el numero dos");
+        double numero2 = sc.nextDouble();
+
+        double resultado = numero1 + numero2;
+
+        System.out.println("la suma de" + numero1 + "+" +  + "=" +resultado);
+
     }
 }
